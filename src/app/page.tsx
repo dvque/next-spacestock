5169a00a10089380f48ae9d86abd014ff3e9a552
dvque <dvdquerol@gmail.com>
2024-01-17 21:54:34 +0100

    update page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,17 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      SpaceStock
+    <main className="flex flex-row gap-4">
+      <div className="flex flex-col basis-1/5">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
+      <div className="flex flex-col basis-1/2">
+        <div>01</div>
+        <div>02</div>
+        <div>03</div>
+      </div>
     </main>
   )
 }
