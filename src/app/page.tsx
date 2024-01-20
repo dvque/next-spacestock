94768985cf352f601829c66fb25e196eeb16fff8
dvque <dvdquerol@gmail.com>
2024-01-20 17:51:06 +0100

    Refactor page layout in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex flex-row gap-4">
-      <div className="flex flex-col basis-1/5">
-        <div>01</div>
-        <div>02</div>
-        <div>03</div>
-      </div>
-      <div className="flex flex-col basis-1/2">
-        <div>01</div>
-        <div>02</div>
-        <div>03</div>
-      </div>
-    </main>
+    <>
+      <p>Hello from home!</p>
+
+    </>
   )
 }
